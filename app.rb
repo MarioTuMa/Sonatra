a92799a7ac0c1e823817e8e061156732a5e8e75e
Marioisdabest <mtutuncumacias1@gmail.com>
2017-04-28 21:05:20 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require "sinatra"
 
 get "/" do
@@ -8,9 +7,4 @@ end
 get "/offersfrombrad.json" do
   content_type :json
   File.read("offersfrombrad.json");
-=======
-require 'sinatra'
-get '/txt' do
-    'Put this in your pipe & smoke it!'
->>>>>>> origin/master
 end
